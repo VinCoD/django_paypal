a610c1fbef191fff8e4b501757cbf09d63b09054
VinCoD <5895.2020@students.ku.ac.ke>
2023-05-27 20:49:27 +0300

    Added dummy model to test paypal functionality

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Payment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    amount = models.DecimalField(max_digits=100, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
