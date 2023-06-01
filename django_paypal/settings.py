f58a002c8b983288d0e849cf010e58ae3d551dbb
VinCoD <5895.2020@students.ku.ac.ke>
2023-06-01 11:08:51 +0300

    modified settings.py for different staticfiles settings

--- django_paypal/settings.py
+++ django_paypal/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,8 +122,8 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 # STATIC_URL = "/static/"
-# STATICFILES_DIRS = [BASE_DIR / "static"] # new
-# STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #new
 
 # Static files (CSS, JavaScript, Images)
